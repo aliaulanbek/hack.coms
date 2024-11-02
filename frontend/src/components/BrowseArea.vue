3d4fcac7ba11ace61671375a13f5f8c59afcdfcb
aliaulanbek <aliakg0604@gmail.com>
2024-11-02 13:53:20 -0400

    initial browse

--- frontend/src/components/BrowseArea.vue
+++ frontend/src/components/BrowseArea.vue
@@ -1,13 +1,46 @@
+<template>
+  <div>
+    <nav class="navbar">
+        <div class="brand">Mentor Database</div>
+      
+        <div class="nav-links">
+            <a href="#students">Students</a>
+            <a href="#alumni">Alumni</a>
+            <a href="#professionals">Professionals</a>
+        </div>
+    </nav>
+  </div>
+</template>
+
 <script setup>
 
 </script>
 
-<template>
-  <div>
-    <h1>Browse page</h1>
-  </div>
-</template>
-
 <style scoped>
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
 
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #333;
+  padding: 1rem;
+  color: white;
+}
+.navbar .brand {
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+.navbar .nav-links a {
+    color: white;
+    text-decoration: none;
+    margin-left: 1rem;
+}
+.navbar .nav-links a:hover {
+    color: #ddd;
+}
 </style>
