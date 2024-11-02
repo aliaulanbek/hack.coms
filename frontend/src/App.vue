310a3c6be9d6f826f8565ba4e3ae08e1281c1e8c
Dubomb <dubomb1011@gmail.com>
2024-11-02 13:53:04 -0400

    Aligned chat and browse areas in a column

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
 import ChatArea from './components/ChatArea.vue';
 import BrowseArea from './components/BrowseArea.vue';
 </script>
 
 <template>
-  <div>
-    <ChatArea></ChatArea>
-    <BrowseArea></BrowseArea>
+  <div class="two-column">
+    <ChatArea class="chat-area"></ChatArea>
+    <BrowseArea class="browse-area"></BrowseArea>
   </div>
 </template>
 
 <style scoped>
+.two-column {
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  height: 100%;
+}
 
+.chat-area {
+  flex: 1;
+}
+
+.browse-area {
+  flex: 3;
+}
 </style>
