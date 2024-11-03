f931a6d0352e581e4732764809ae005ceaf45a40
Dubomb <dubomb1011@gmail.com>
2024-11-02 23:07:49 -0400

    Migrated App.vue items to a Homepage.vue for clarity

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-
-import ChatArea from './components/ChatArea.vue';
-import BrowseArea from './components/BrowseArea.vue';
-import MessageArea from './components/MessageArea.vue';
 import SignInPage from './components/SignInPage.vue';
+import HomePage from './HomePage.vue';
 
 import { ref } from 'vue';
-import Profile from './components/Profile.vue';
-import EditProfile from './components/EditProfile.vue';
 
-const isMessaging = ref(false);
 let signedIn = ref(false);
 let currentUser = ref(null);
-const isEditProfile = ref(true);
-
-function toggleMessage() {
-  isMessaging.value = !isMessaging.value;
-  console.log(isMessaging);
-}
 
 function signIn(user) {
   currentUser.value = user;
@@ -29,64 +17,11 @@ function signIn(user) {
   <div v-if="!signedIn">
     <SignInPage @signin-event="signIn"></SignInPage>
   </div>
-  <div v-else-if="signedIn">
-    <nav class="navbar">
-      <div class="brand">ALANA.CONNECT</div>
-      <div class="nav-options">
-        <button @click="toggleEditProfile">Edit Profile</button>
-      </div>
-    </nav>
-    <div class="two-column" v-if="isEditProfile">
-      <ChatArea @toggle-event="toggleMessage" class="chat-area"></ChatArea>
-      <MessageArea @toggle-event="toggleMessage" v-if="isMessaging" class="browse-area"></MessageArea>
-      <BrowseArea @toggle-event="toggleMessage" v-else-if="!isMessaging" class="browse-area"></BrowseArea>
-    </div>
+  <div v-else>
+    <HomePage></HomePage>
   </div>
-  <div v-if="!isEditProfile">
-    <EditProfile></EditProfile>
-  </div>
-  <!-- <div v-else>
-    <h1>Sign in here</h1>
-  </div> -->
 </template>
 
 <style scoped>
-.two-column {
-  display: flex;
-  gap: 20px;
-  width: 100%;
-  height: 100vh;
-  margin: auto;
-}
 
-.chat-area {
-  flex: 1;
-}
-
-.browse-area {
-  flex: 3;
-}
-
-.navbar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #333;
-  padding: 1rem;
-  color: white;
-  margin-bottom: 15px;
-  border-radius: 10px;
-}
-.navbar .brand {
-  font-size: 1.5rem;
-  font-weight: bold;
-}
-.navbar .nav-options button {
-  color: white;
-  text-decoration: none;
-  margin-left: 1rem;
-}
-.navbar .nav-options button:hover {
-  color: #ddd;
-}
 </style>
---
+++ frontend/src/HomePage.vue
@@ -0,0 +1,78 @@
+<script setup>
+import ChatArea from './components/ChatArea.vue';
+import BrowseArea from './components/BrowseArea.vue';
+import MessageArea from './components/MessageArea.vue';
+import EditProfile from './components/EditProfile.vue';
+import Profile from './components/Profile.vue';
+
+import { ref } from 'vue';
+
+const isEditProfile = ref(false);
+
+const isMessaging = ref(true);
+
+function toggleMessage() {
+  isMessaging.value = !isMessaging.value;
+  console.log(isMessaging);
+}
+</script>
+
+<template>
+    <div>
+        <nav class="navbar">
+            <div class="brand">ALANA.CONNECT</div>
+            <div class="nav-options">
+                <button @click="toggleEditProfile">Edit Profile</button>
+            </div>
+        </nav>
+        <div class="two-column">
+            <ChatArea @toggle-event="toggleMessage" class="chat-area"></ChatArea>
+            <MessageArea @toggle-event="toggleMessage" v-if="isMessaging" class="browse-area"></MessageArea>
+            <BrowseArea @toggle-event="toggleMessage" v-else-if="!isMessaging" class="browse-area"></BrowseArea>
+        </div>
+    </div>
+
+    <div v-if="isEditProfile">
+        <EditProfile></EditProfile>
+    </div>
+</template>
+
+<style scoped>
+.two-column {
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  height: 100vh;
+  margin: auto;
+}
+
+.chat-area {
+  flex: 1;
+}
+
+.browse-area {
+  flex: 3;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #333;
+  padding: 1rem;
+  color: white;
+  margin-bottom: 15px;
+  border-radius: 10px;
+}
+.navbar .brand {
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+.navbar .nav-options button {
+  color: white;
+  text-decoration: none;
+  margin-left: 1rem;
+}
+.navbar .nav-options button:hover {
+  color: #ddd;
+}
+</style>
